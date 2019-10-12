17d150b6cf5d687fa8caed7dc1c17990b3bdccb1
Relin <1101005304@qq.com>
2019-10-12 14:43:40 +0800

    AliPayListener modify

--- app/src/main/java/com/android/pay/wxlogin/OnWXLoginListener.java
+++ app/src/main/java/com/android/pay/wxlogin/OnWXLoginListener.java
@@ -6,10 +6,13 @@ package com.android.pay.wxlogin;
 public interface OnWXLoginListener {
 
 
-    /***
+    /**
      * 微信登录回调信息
-     * @param user 用户信息
+     *
+     * @param code
+     * @param msg
+     * @param user
      */
-    void onWXLogin(int code,String msg,WXUser user);
+    void onWXLogin(int code, String msg, WXUser user);
 
 }
